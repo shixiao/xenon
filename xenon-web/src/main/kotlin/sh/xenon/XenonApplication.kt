56fcb2a7f1a0a52fedccb6015d2610385a3d993d
Scott Pruett <spruett345@gmail.com>
2017-12-27 17:39:13 -0500

    [web] Add metrics

--- xenon-web/src/main/kotlin/sh/xenon/XenonApplication.kt
+++ xenon-web/src/main/kotlin/sh/xenon/XenonApplication.kt
@@ -1,14 +1,31 @@
 package sh.xenon
 
+import com.codahale.metrics.ConsoleReporter
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.jooby.Kooby
 import org.jooby.json.Jackson
+import org.jooby.metrics.Metrics
 import org.jooby.run
 import sh.xenon.resources.TestResource
+import java.util.concurrent.TimeUnit
 
 class XenonApplication : Kooby({
     // Modules
     use(Jackson(jacksonObjectMapper()))
+    use(Metrics()
+            .metric("memory", MemoryUsageGaugeSet())
+            .metric("threads", ThreadStatesGaugeSet())
+            .metric("gc", GarbageCollectorMetricSet())
+            .request()
+            .reporter({ registry ->
+                val reporter = ConsoleReporter.forRegistry(registry).build()
+                reporter.start(1, TimeUnit.MINUTES)
+                reporter
+            })
+    )
 
     // Resources
     use(TestResource::class)
